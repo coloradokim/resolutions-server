96f3168a3c4b7032b8376b225d115e7047659c1f
Casey Herold <caseynate@gmail.com>
2018-01-18 19:07:41 -0700

    initial data structure setup

---
+++ data/posting.js
@@ -0,0 +1,25 @@
+module.exports = [{
+	"id": 1,
+	"title": "PBR Can",
+	"price": 1.00,
+	"location": "Limon Co.",
+	"description": "",
+	"size": 1,
+	"spitefulness": 3,
+	"ghostProofing": 1,
+	"imageUrl": "http://www.skyscraperbrewing.com/images/GABF06/pbr01.jpg",
+	"email": "hill-billy-bob-@yahoo.com",
+},
+
+{
+	"id": 2,
+	"title": "PBR Can",
+	"price": 1.00,
+	"location": "Limon Co.",
+	"description": " ",
+	"size": 1,
+	"spitefulness": 3,
+	"ghostProofing": 1,
+	"imageUrl": " ",
+	"email": "",
+}]
