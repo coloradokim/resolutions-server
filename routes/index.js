f25d8f4bdac2084bd0c288031cf0e7a64a04abf5
coloradokim <schlesinger.kim@gmail.com>
2018-01-18 20:08:58 -0700

    add post functionality

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 var postings = require('../data/posting');
 
 router.get('/', function(req, res, next) {
-  res.json(postings);
+  res.status(200).json(postings);
+});
+
+router.post('/', function(req, res, next) {
+  res.status(201);
+  res.json({
+    data: {
+      message: 'Posting Recieved!'
+    }
+  });
 });
 
 module.exports = router;
